181bef770fb1f14680cd18310aaf95127f666003
Matthew Franglen <matthew@brandwatch.com>
2015-10-27 17:06:24 +0000

    Try to make an integration test

--- Makefile
+++ Makefile
@@ -16,16 +16,3 @@ include $(PGXS)
 
 SHLIB_LINK += $(filter -lz -lpthread -lrt, $(LIBS))
 SHLIB_LINK += -lrdkafka
-
-TEST_LIBRARY_OBJECTS = tests/tap/basic.h tests/tap/basic.o tests/tap/float.h tests/tap/float.o tests/tap/macros.h
-
-test : build-tests
-	runtests -b tests -s tests -l tests/TESTS
-
-build-tests : tests/kafka_fdw.t.o
-	$(CC) -I. $(CFLAGS) $(PG_LIBS) $(LDFLAGS) $(LDFLAGS_EX) $(LIBS) -o tests/kafka_fdw-t tests/kafka_fdw.t.o $(TEST_LIBRARY_OBJECTS)
-
-tests/kafka_fdw.t.o : $(TEST_LIBRARY_OBJECTS)
-
-clean :
-	rm tests/*-t tests/*.o tests/tap/*.o
--- sql/kafka_fdw.sql
+++ sql/kafka_fdw.sql
@@ -1,14 +1,24 @@
-drop extension kafka_fdw cascade;
-create extension kafka_fdw;
-create server kafka_openstack_alexey_1 foreign data wrapper kafka_fdw options (host '10.2.134.65', port '9092');
-create foreign table kafka_openstack_alexey_1_elephants(kafka_offset bigint, kafka_value text) server kafka_openstack_alexey_1 options (topic 'test', batch_size '30000', offset '26528300');
+CREATE EXTENSION kafka_fdw;
 
-select * from kafka_openstack_alexey_1_elephants limit 5;
-select * from kafka_openstack_alexey_1_elephants limit 5;
-explain select * from kafka_openstack_alexey_1_elephants limit 5;
-explain analyze select * from kafka_openstack_alexey_1_elephants limit 100000;
+CREATE SERVER s_kafka
+FOREIGN DATA WRAPPER kafka_fdw
+OPTIONS ();
 
---explain select * from kafka_openstack_alexey_1_elephants cross join kafka_openstack_alexey_1_elephants b;
---select * from kafka_openstack_alexey_1_elephants cross join kafka_openstack_alexey_1_elephants b;
+CREATE FOREIGN TABLE t_kafka
+(
+    i_offset bigint,
+    t_value text
+)
+SERVER s_kafka
+OPTIONS
+(
+    topic 'test',
+    batch_size: '30000'
+);
 
---\des+ kafka_openstack_alexey_1
+SELECT
+    i_offset,
+    t_value
+FROM
+    t_kafka
+LIMIT 1;
