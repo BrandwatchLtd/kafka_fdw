0881fe0096bcd1374cf0896549acec64a40bb0ac
Alexey Bashtanov <alexey@brandwatch.com>
2015-10-28 08:51:53 +0000

    changed SQL to test with integration

--- sql/kafka_fdw.sql
+++ sql/kafka_fdw.sql
@@ -1,7 +1,7 @@
 drop extension kafka_fdw cascade;
 create extension kafka_fdw;
 create server kafka_openstack_alexey_1 foreign data wrapper kafka_fdw options (host '10.2.134.65', port '9092');
-create foreign table kafka_openstack_alexey_1_valid(kafka_offset bigint, kafka_value text) server kafka_openstack_alexey_1 options (topic 'test', batch_size '30000', offset '26528300');
+create foreign table kafka_openstack_alexey_1_valid(kafka_offset bigint, kafka_value text) server kafka_openstack_alexey_1 options (topic 'gnip.twitter', batch_size '30000', offset '-2');
 select * from kafka_openstack_alexey_1_valid limit 5;
 select * from kafka_openstack_alexey_1_valid limit 5;
 explain select * from kafka_openstack_alexey_1_valid limit 5;
